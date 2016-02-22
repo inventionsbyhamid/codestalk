2ef9d7758ed76a3c6b9ecc95b424db30fc42b222
Hamid Siddiqui <inventionsbyhamid@gmail.com>
2016-02-22 19:04:03 +0530

    from email fix,environment variable in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,8 +48,8 @@ Rails.application.configure do
   :address => "smtp.sendgrid.net",
   :port => 587,
   :domain => "heroku.com",
-  :user_name => Rails.application.secrets.sendgrid_username,
-  :password => Rails.application.secrets.sendgrid_password,
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
   :authentication => :plain,
   :enable_starttls_auto => true
 }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://codechef.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://codestalk.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_options = {from: 'inventionsbyhamid@gmail.com'}
